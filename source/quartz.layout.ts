6dfbc7cf1fee18981921b3f8ca8bb6c4642d35b5
l-s-campos <ufeslsc@gmail.com>
2025-08-15 12:04:21 -0300

    vault backup: 2025-08-15 12:04:21

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -25,12 +25,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly({
-      title: "Lista", // title of the explorer component
-      folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-      folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
-      useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
-    }),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
